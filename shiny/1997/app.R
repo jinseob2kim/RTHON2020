31def34fc699a4fe1271eb489970b662b9ce6a88
Jinseob Kim <jinseob2kim@gmail.com>
2020-11-28 23:42:12 +0900

    s

--- shiny/1997/app.R
+++ shiny/1997/app.R
@@ -1,31 +1,20 @@
-#install.packages("stringr")
-#install.packages("dplyr")
-#install.packages('tidyverse')
-#install.packages("shiny")
 options(shiny.sanitize.errors = F) ## Error 내용 확인가능
 
 library(shiny)
 library(ggplot2)
 library(dplyr)
-#library(tqk)
-#library(tidyverse) # 8개의 package가 있음
-#library(httr)
-#library(rvest)
-#library(jsonlite)
-#library(stringr)
-
 library(showtext)
 font_add_google(name = "Nanum Gothic")
 showtext_auto()
 showtext_opts(dpi = 100)
 
-data <- readRDS(file = 'long_1128_1.RDS')#데이터 파일 불러오기
-company<-'LG화학'
+data <- readRDS('long_1128_1.RDS')#데이터 파일 불러오기
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
   # Application title
-  titlePanel("company salse analysis"),
+  titlePanel("company sales analysis"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
@@ -39,32 +28,32 @@ ui <- fluidPage(
 
     # Show a plot of the generated distribution
     mainPanel(
-      uiOutput("tb")
-    )
+      tabsetPanel(
+        tabPanel("barplot",
+                 textOutput("value"),
+                 plotOutput("barplot")))
+      )
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$value <- renderPrint({ input$text })
-  output$barplot <- reactivePlot(function(){
 
-    company <-input$text
+  output$value <- renderText({ input$text })
+
+  output$barplot <- renderPlot({
 
     p <- data %>%
-      filter(회사 == company) %>%
+      filter(회사 == input$text) %>%
       ggplot(aes(x = year, y = sales , fill = year)) +
       geom_col() +
       ggtitle("[실적]")+
       theme_bw()
-    print(p)
+
+    return(p)
   })
 
-  output$tb <- renderUI({
 
-    tabsetPanel(tabPanel("barplot",plotOutput("barplot")))
-
-  })
 }
 
 # Run the application
