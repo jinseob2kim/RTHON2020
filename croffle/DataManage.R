36d45cc2ff6a08ea46d206bd1daa20e19fb2dc22
Jinseob Kim <jinseob2kim@gmail.com>
2020-11-29 11:30:33 +0900

    s

--- croffle/DataManage.R
+++ croffle/DataManage.R
@@ -1,6 +1,6 @@
 ## Set directory & load packages
 setwd("~/Lecture/RTHON2020/croffle/data")
-library(data.table)  ## rbindlist
+library(plyr)        ## rbind.fill
 library(readxl)      ## read_excel
 library(magrittr)    ## %>%
 
@@ -12,12 +12,16 @@ xlsxlist <- list.files(pattern = "xlsx")
 
 ## Read csv
 
-all.csv <- lapply(csvlist, read.csv, check.names = F) %>% rbindlist(fill = T)   ## check.names = F, 원래컬럼이름 그대로(. 없음)
+all.csv <- lapply(csvlist, read.csv, check.names = F) %>% rbind.fill   ## check.names = F, 원래컬럼이름 그대로(. 없음)
 all.csv$`합계 길이` <- as.numeric(gsub('\\([^)]*\\)', "", all.csv$`합계 길이`))  ## 괄호안 숫자 지우고 숫자형으로
 
-all.xlsx  <- lapply(xlsxlist, read_excel, na = "-") %>% rbindlist(fill = T)
+all.xlsx  <- lapply(xlsxlist, read_excel, na = "-") %>% rbind.fill
 
 
 ## Final
-road <- rbind(all.csv, all.xlsx, fill = T, use.names = T)
+road <- rbind.fill(all.csv, all.xlsx)
 road <- data.frame(road)
+
+
+
+
