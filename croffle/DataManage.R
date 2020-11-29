de41aa88162e668d67a5b2c0c949d4cd7c12971c
Jinseob Kim <jinseob2kim@gmail.com>
2020-11-29 14:17:33 +0900

    s

--- croffle/DataManage.R
+++ croffle/DataManage.R
@@ -20,8 +20,21 @@ all.xlsx  <- lapply(xlsxlist, read_excel, na = "-") %>% rbind.fill
 
 ## Final
 road <- rbind.fill(all.csv, all.xlsx)
-road <- data.frame(road)
+road <- data.frame(road, check.names = F)
 
+
+## 광역자치단체, 시도, 소재지 합쳐서 `지역` 으로
+road$sido <- ifelse(!is.na(road$광역자치단체), road$광역자치단체,
+                  ifelse(!is.na(road$시도), road$시도,
+                         ifelse(!is.na(road$소재지), road$소재지, NA)))
+
+road$지역 <- paste(road$sido, road$시군구, sep = " ")
+road$지역 <- gsub("NA |NA NA", NA, road$지역)   ## NA 처리
+
+
+## 필요없는 열 지우기
+var.exclude <- c("광역자치단체", "시도", "소재지", "시군구", "sido", "...2", "...8")
+road <- road[, setdiff(names(road), var.exclude)]
 
 
 
